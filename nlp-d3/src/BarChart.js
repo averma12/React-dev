0221991b0c7b0a5f79af3647269f02eee5d344e7
Abhinav Verma <abhinav_win12@yahoo.co.in>
2018-07-05 18:02:04 +0530

    create bar chart using d3 and react

--- nlp-d3/src/BarChart.js
+++ nlp-d3/src/BarChart.js
@@ -1,7 +1,8 @@
 import React , {Component} from 'react';
-import { scaleLinear } from 'd3-scale';
+import { scaleLinear,append, } from 'd3-scale';
 import { max } from 'd3-array';
 import { select } from 'd3-selection';
+
 
 class BarChart extends Component{
 
@@ -9,17 +10,26 @@ class BarChart extends Component{
         super(props);
         this.node = React.createRef();
         this.state = {
-
+             size:{
+                 width:0,
+                 height:0
+             }
         }
     }
 
     static getDerivedStateFromProps(nextProps, prevState) {
         console.log("getDerivedStateFromProps from Child");
         console.log(nextProps);
+       
         console.log(prevState);
-        return null;
+        return {size: {
+            width: nextProps.size[0],
+            height: nextProps.size[1]
+        }};
    }
  
+    
+
     componentDidMount() {
         this.createBarChart()
      }
@@ -29,21 +39,54 @@ class BarChart extends Component{
 
      createBarChart = () => {
          console.log("Hello",this.node.current);
-         select(this.node.current)
-         .selectAll('rect')
-         .enter()
-         .append('rect');
+         const barPadding = 5;
+         const barWidth = (this.state.size.width/this.props.data.length);
+         console.log(barWidth);
+         
          console.log(this.props.data);
+         const dataMax = max(this.props.data)
+         const yScale = scaleLinear()
+            .domain([0, dataMax])
+            .range([0, this.state.size.height]);
+
+
+            select(this.node.current)
+            .selectAll('rect')
+            .data(this.props.data)
+            .enter()
+            .append('rect')
+         
+         select(this.node.current)
+            .selectAll('rect')
+            .data(this.props.data)
+            .exit()
+            .remove()
+
+            select(this.node.current)
+            .selectAll('rect')
+            .data(this.props.data)
+            .style('fill', '#fe9922')
+            .attr('x', (d,i) => i * 25)
+            .attr('y', d => this.props.size[1] - yScale(d))
+            .attr('height', d => yScale(d))
+            .attr('width', 25);
+         
 
      }
 
      render() {
-        return <svg ref={this.node}
-        width={500} height={500}>
+        return (
+        
+        <svg ref={this.node}
+        width={this.state.size.width} height={this.state.size.height}>
         </svg>
+        )
      }
 
 
 }
 
 export default BarChart;
+function newFunction_1(nextProps) {
+    BarChart.newFunction(nextProps);
+}
