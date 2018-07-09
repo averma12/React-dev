e3547358015c7bd4c615419812c65a930a18549c
Abhinav Verma <abhinav_win12@yahoo.co.in>
2018-07-09 23:00:54 +0530

    Add enzyme test cases

--- reacttest/src/App.test.js
+++ reacttest/src/App.test.js
@@ -1,27 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
 import App from "./App";
+import CommentBox from "./Components/CommentBox";
+import CommentList from "./Components/CommentList";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
-  console.log(div.innerHTML,"hello");
+  
   expect(div.firstChild.innerHTML).toContain("Hi there!");
   ReactDOM.unmountComponentAtNode(div);
 });
 
 it("shows a comment box", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  console.log(div.innerHTML);
-  expect(div.innerHTML).toContain("Comment Box");
-  ReactDOM.unmountComponentAtNode(div);
+  const wrapped =  shallow(<App /> );
+  expect(wrapped.find(CommentBox).length).toEqual(1);
 });
 
 it("shows a comment list", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  console.log(div.innerHTML);
-  expect(div.innerHTML).toContain("CommentList");
-  ReactDOM.unmountComponentAtNode(div);
+  const wrapped =  shallow(<App /> );
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });
