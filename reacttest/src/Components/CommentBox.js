b8eb3c458d32fb5c7908319ec705a1501ea1fba7
Abhinav Verma <abhinav_win12@yahoo.co.in>
2018-07-11 23:51:27 +0530

    Adding Full dom rendering example

--- reacttest/src/Components/CommentBox.js
+++ reacttest/src/Components/CommentBox.js
@@ -1,5 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default  () => {
-    return <div>Comment Box</div>;
-};
+class CommentBox extends Component {
+    state = {
+        text: "hello",
+    };
+    
+    handleChange = (event) => {
+        this.setState({ text: event.target.value });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({ text: "" });
+     }
+
+    render() {
+       return (
+           <form onSubmit={this.handleSubmit}>
+            <h4>
+            Enter CommentBox
+            </h4>
+           <textarea onChange={this.handleChange} value={this.state.text} />
+            
+           <div>
+               <button>
+                   Submit
+               </button>
+            </div>
+           </form>
+       );
+    }
+}
+
+
+export default CommentBox;
---
+++ reacttest/src/Components/__tests__/CommentBox.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import CommentBox from "Components/CommentBox";
+
+import { shallow } from "enzyme";
+
+it(" shows a comment box and a button ", () => {
+    
+})
