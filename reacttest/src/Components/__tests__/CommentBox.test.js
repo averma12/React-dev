e11b48ef53901de80f26add45633b32c725b6ff0
Abhinav Verma <abhinav_win12@yahoo.co.in>
2018-07-12 23:16:32 +0530

    Full Component mounting

--- reacttest/src/Components/__tests__/CommentBox.test.js
+++ reacttest/src/Components/__tests__/CommentBox.test.js
@@ -2,8 +2,20 @@ import React from "react";
 
 import CommentBox from "Components/CommentBox";
 
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
+
+let wrapped;
+
+beforeEach(() => {
+    wrapped = mount(<CommentBox />);
+});
+
+
+afterEach(() => {
+    wrapped.unmount();
+});
 
 it(" shows a comment box and a button ", () => {
-    
+ expect(wrapped.find("textarea").length).toEqual(1);
+ expect(wrapped.find("button").length).toEqual(1);
 })
