2bfa8e41d00be8cffba0f3fa7cfb489639c96d6e
Abhinav Verma <abhinav_win12@yahoo.co.in>
2018-07-15 19:36:07 +0530

    Testing out form submits and adding describe block to group conditions

--- reacttest/src/Components/__tests__/CommentBox.test.js
+++ reacttest/src/Components/__tests__/CommentBox.test.js
@@ -20,9 +20,25 @@ it(" shows a comment box and a button ", () => {
  expect(wrapped.find("button").length).toEqual(1);
 });
 
-it("has a text area that users can type in", () => {
-wrapped.find("textarea").simulate("change", {
-    target: { value: "new comment" },
+
+describe("the text area", () => {
+
+beforeEach( () => {
+    wrapped.find("textarea").simulate("change", {
+        target: { value: "new comment" },
+    });
+    wrapped.update();
 });
-wrapped.update();
+
+it("has a text area that users can type in", () => {
+
+expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
+});
+
+it("clear out text area when form is submitted", () => {
+
+wrapped.find("form").simulate("submit", {
+   target: { value: "" },
+});
+});
 });
