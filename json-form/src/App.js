e999764b13c156cdaa841cfe9e0fe92985e7efdf
Abhinav Verma <abhinav_win12@yahoo.co.in>
2018-12-11 12:55:49 +0530

    Basic form rendered dynamically

--- json-form/src/App.js
+++ json-form/src/App.js
@@ -18,10 +18,39 @@ class App extends Component {
 
     };
   }
+
+  onSubmit = (model) => {
+    console.log("ye model hai",model);
+  }
   render() {
     return (
       <div className="App">
-        <DynamicForm/>
+        <DynamicForm className = "form"
+        title = "Registration"
+          
+          model={[
+            {key: "name", label: "Name", props: {required: true}},
+            {key: "age",label: "Age", type: "number"},
+            {key: "rating",label: "Rating", type: "number", props:{min:0,max:5}},
+            {key: "gender",label: "Gender", type:"radio",options:[
+              {key:"male",label:"Male",name:"gender",value:"male"},
+              {key:"female",label:"Female",name: "gender",value:"female"}
+            ]},
+            {key: "qualification",label: "Qualification"},
+            {key: "city",label:"City", type:"select", value: "Kerala", options: [
+                {key:"mumbai",label:"Mumbai",value:"Mumbai"},
+                {key:"bangalore",label:"Bangalore",value:"Bangalore"},
+                {key:"kerala",label:"Kerala",value:"Kerala"},
+            ]},
+            {key: "skills",label:"Skills", type:"checkbox", options: [
+                {key:"reactjs",label:"ReactJS",value:"reactjs"},
+                {key:"angular",label:"Angular",value:"angular"},
+                {key:"vuejs",label:"VueJS",value:"vuejs"},
+            ]},
+
+          ]}
+          onSubmit = {(model) => {this.onSubmit(model)}} 
+        />
       </div>
     );
   }
--- json-form/src/DynamicForm.js
+++ json-form/src/DynamicForm.js
@@ -13,11 +13,46 @@ componentDidMount(){
     console.log(this.props);
 }
 
+onSubmit = (event) => {
+    event.preventDefault();
+    if (this.props.onSubmit) {
+      this.props.onSubmit(this.state);
+    }
+
+}
+
+renderForm = (event) => {
+    let model = this.props.model;
+    let form = model.map(m => {
+        let key = m.key;
+        let type = m.type;
+        let props = m.props || {};
+        
+        return (
+            <div key={key} className="form-group">
+            <label className="form-label"
+            key={`l-${key}`}
+            htmlFor={key}>
+            {m.label}
+            </label>
+
+            </div>
+        );
+    })
+    return form;
+}
+
 
 render(){
     return(
-        <div>
-            So dynamic bro
+        <div className={this.props.className}>
+            <h3 className="form-title">{this.props.title}</h3>
+                <form className="dynamic-form" onSubmit={(e)=>{this.onSubmit(e)}}>
+                    {this.renderForm()}
+                    <div className="form-actions">
+                        <button type="submit">submit</button>
+                    </div>
+                </form>
             </div>
     )
 }
